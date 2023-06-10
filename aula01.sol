a5453361c39ae156e3e0b37ba209cc24f50974fb
dkptor <136171262+dkptor@users.noreply.github.com>
2023-06-10 10:43:41 -0300

    Create aula01.sol

---
+++ aula01.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract OlaMundo7COMm {
+    function olaMundo() public pure returns (uint256){
+        return 100;
+    }
+    
+    function helloWorld() public pure returns (bool){
+        return true;
+    }
+}
+//0xd9145CCE52D386f254917e481eB44e9943F39138
