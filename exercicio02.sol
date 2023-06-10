f8e004bced2545ba5e3ecd7031b9791efe766cf1
dkptor <136171262+dkptor@users.noreply.github.com>
2023-06-10 15:15:00 -0300

    Update exercicio02.sol

--- exercicio02.sol
+++ exercicio02.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.4;
 contract Exercicio02 {
     string public nomeLocador;
     string public nomeLocatario;
-    uint8[36] public valorAluguel36Meses;
+    uint256[36] public valorAluguel36Meses;
 
     constructor(string memory _nomeLocador,
                 string memory _nomeLocatario,
-                uint8 _valorAluguelInicial)  {
+                uint256 _valorAluguelInicial)  {
 
         uint8 i;
         
@@ -20,7 +20,7 @@ contract Exercicio02 {
     }
 
     function showValorALuguelByMes(uint8 _mes) public view returns (uint256) {
-        return valorAluguel36Meses[_mes];
+        return valorAluguel36Meses[_mes -1];
     }
 
     function showNomesDoLocadorELocatario() public view returns (string memory, string memory) {
@@ -36,13 +36,13 @@ contract Exercicio02 {
         return true;
     }    
 
-    function setReajusteAluguel(uint8 _mesAtual, uint8 _valorReajuste) public returns (bool) {
+    function setReajusteAluguel(uint8 _mesAtual, uint256 _valorReajuste) public returns (bool) {
         uint8 i;
         for (i=_mesAtual;i<36;i++){
-            valorAluguel36Meses[i] += _valorReajuste;
+            valorAluguel36Meses[i] = valorAluguel36Meses[i] + _valorReajuste;
         }
         return true;
     }        
 }
 //Exercicio02.(constructor) 
-//Contrato: 0x9f1dc4d1b87b71024c2F5A647e7C30775D708515
+//Contrato: 0x18b186545aD5B4016F3D62cB2526752bD5B2DC83
