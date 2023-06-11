cf36b968c114917ec11834085af98e54a83cb11f
dkptor <136171262+dkptor@users.noreply.github.com>
2023-06-10 22:21:15 -0300

    Update exercicio03.sol

--- exercicio03.sol
+++ exercicio03.sol
@@ -1,15 +1,20 @@
-// Modifier
-// Revert
-// Struct
-// Require
+// Modifier 
+// Revert ok
+// Struct ok
+// Require ok
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.12;
 
 contract Exercicio03 {
-    string public nomeLocador;
-    string public nomeLocatario;
-    uint256[36] public valorAluguel36Meses;
+
+    struct dadosAluguel {
+        string nomeLocador;
+        string nomeLocatario;
+        uint256[36] valorAluguel36Meses;
+    }
+
+    dadosAluguel public contrato;
 
     constructor(string memory _nomeLocador,
                 string memory _nomeLocatario,
@@ -17,37 +22,65 @@ contract Exercicio03 {
 
         uint8 i;
         
-        nomeLocador = _nomeLocador;
-        nomeLocatario = _nomeLocatario;
+        contrato.nomeLocador = _nomeLocador;
+        contrato.nomeLocatario = _nomeLocatario;
         for (i=0;i<36;i++){
-            valorAluguel36Meses[i] = _valorAluguelInicial;
+            contrato.valorAluguel36Meses[i] = _valorAluguelInicial;
         }
     }
 
-    function showValorALuguelByMes(uint8 _mes) public view returns (uint256) {
-        return valorAluguel36Meses[_mes -1];
+    modifier onlyValidNumber(uint _number, string memory _descricao) {
+        require(_number > 0, string.concat(_descricao, " invalido !"));
+        _;
     }
 
-    function showNomesDoLocadorELocatario() public view returns (string memory, string memory) {
-        return (nomeLocador, nomeLocatario);
+    modifier onlyValidString(string memory _value, string memory _descricao) {
+        require(keccak256(bytes(_value)) != keccak256(""), string.concat(_descricao, " invalido !"));
+        _;
     }    
 
-    function setNameByType(uint8 _type, string calldata _valorNovoNome) public returns (bool) {
-        if (_type == 1){
-            nomeLocador = _valorNovoNome;  
-        } else if (_type == 2) {
-            nomeLocatario = _valorNovoNome;
+    function showValorALuguelByMes(uint8 _mes) 
+        public view 
+        onlyValidNumber(_mes, "Mes") 
+        returns (uint256) {
+            return contrato.valorAluguel36Meses[_mes -1];
+    }
+
+    function showNomesDoLocadorELocatario() 
+        public view 
+        returns (string memory, string memory) {
+            return (contrato.nomeLocador, contrato.nomeLocatario);
+    }    
+
+    function setNameByType(uint8 _type, string calldata _valorNovoNome) 
+        public 
+        onlyValidString(_valorNovoNome, "Novo nome")
+        returns (bool) {
+            require(_type == 1 || _type == 2, "Type informado invalido!");
+            if (_type == 1){
+                contrato.nomeLocador = _valorNovoNome;  
+            } else if (_type == 2) {
+                contrato.nomeLocatario = _valorNovoNome;
+            }
+            return true;
+    }    
+
+    function setReajusteAluguelAposMesInformado(uint8 _mesInformado, uint256 _valorReajuste) 
+    public
+    onlyValidNumber(_mesInformado, "Mes")
+    onlyValidNumber(_valorReajuste, "Valor")
+    returns (bool) {
+        if (_mesInformado > 36) {
+            revert("Mes informado nao esta no contrato");
         }
-        return true;
-    }    
 
-    function setReajusteAluguel(uint8 _mesAtual, uint256 _valorReajuste) public returns (bool) {
         uint8 i;
-        for (i=_mesAtual;i<36;i++){
-            valorAluguel36Meses[i] = valorAluguel36Meses[i] + _valorReajuste;
+        for (i=_mesInformado;i<36;i++){
+            contrato.valorAluguel36Meses[i] = contrato.valorAluguel36Meses[i] + _valorReajuste;
         }
         return true;
     }        
 }
-//Exercicio02.(constructor) 
-//Contrato: 
+//Exercicio03.(constructor) 
+//Contrato: 0x5e06fdfd9dbd2ffb2e9def64265769a26796c105
+//Transaction: 0xab29d1f083bf7f3e8fde01ebca0082e0275bb4c0a601e20718693e654c649722
